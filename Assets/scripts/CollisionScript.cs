7b0f5074ea94a8a3f93dab03feb73e84a53f5035
OsamaAldawoody <frouzen1650@gmail.com>
2020-02-23 10:05:33 +0200

    Initial commit

---
+++ Assets/scripts/CollisionScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour
+{
+    int score = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision");
+        
+        if (collision.gameObject.tag == "Coin")
+        {
+            score++;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.tag == "Bag")
+        {
+           
+        }
+    }
+}
